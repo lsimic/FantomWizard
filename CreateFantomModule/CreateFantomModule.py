d8b3e526cd5dda08c7bcb576709080495fac21a7
Luka Simic <luka.simic51@gmail.com>
2026-03-30 10:30:26 +0200

    switch to tab layout for new functionality

--- CreateFantomModule/CreateFantomModule.py
+++ CreateFantomModule/CreateFantomModule.py
@@ -108,43 +108,16 @@ class CreateFantomModuleParameterNode:
   voxelSizeCoronal: Annotated[float, WithinRange(0.2, 2.0)] = 1.0
   voxelSizeAxial: Annotated[float, WithinRange(0.2, 2.0)] = 1.0
 
-class CreateFantomModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
-  """Uses ScriptedLoadableModuleWidget base class, available at:
-  https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
-  """
 
-  def __init__(self, parent=None) -> None:
-    """Called when the user opens the module the first time and the widget is initialized."""
-    ScriptedLoadableModuleWidget.__init__(self, parent)
-    VTKObservationMixin.__init__(self)  # needed for parameter node observation
-    self.logic = None
-    self._parameterNode = None
-    self._parameterNodeGuiTag = None
+class GenerateFantomTabWidget:
+  def __init__(self, logic, resourcePath):
+    self.logic = logic
+    self.resourcePath = resourcePath
+    self.widget = slicer.util.loadUI(self.resourcePath('UI/GenerateFantomTab.ui'))
+    self.ui = slicer.util.childWidgetVariables(self.widget)
 
-  def setup(self) -> None:
-    """Called when the user opens the module the first time and the widget is initialized."""
-    ScriptedLoadableModuleWidget.setup(self)
-
-    # Load widget from .ui file (created by Qt Designer).
-    # Additional widgets can be instantiated manually and added to self.layout.
-    uiWidget = slicer.util.loadUI(self.resourcePath("UI/CreateFantomModule.ui"))
-    self.layout.addWidget(uiWidget)
-    self.ui = slicer.util.childWidgetVariables(uiWidget)
-
-    # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
-    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
-    # "setMRMLScene(vtkMRMLScene*)" slot.
-    uiWidget.setMRMLScene(slicer.mrmlScene)
-
-    # Create logic class. Logic implements all computations that should be possible to run
-    # in batch mode, without a graphical user interface.
-    self.logic = CreateFantomModuleLogic()
-
-    # Connections
-
-    # These connections ensure that we update parameter node when scene is closed
-    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
-    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+    # uiWidget = slicer.util.loadUI(self.resourcePath("UI/CreateFantomModule.ui"))
+    # self.layout.addWidget(uiWidget)
 
     # Buttons
     self.ui.applyButton.connect("clicked(bool)", self.onApplyButton)
@@ -161,9 +134,6 @@ class CreateFantomModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
 
     # make sure that the values are in a valid range
     self.onTrimesterOrHeadChange()
-
-    # Make sure parameter node is initialized (needed for module reload)
-    self.initializeParameterNode()
 
     # make sure that the folder selector is correctly enabled/disabled
     self.onExportVoxelizedOrDicomChange()
@@ -182,54 +152,6 @@ class CreateFantomModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
 
     # make sure that the apply button has correct enabling state on init
     self.applyButtonEnabling() 
-
-  def cleanup(self) -> None:
-    """Called when the application closes and the module widget is destroyed."""
-    self.removeObservers()
-
-  def enter(self) -> None:
-    """Called each time the user opens this module."""
-    # Make sure parameter node exists and observed
-    self.initializeParameterNode()
-
-  def exit(self) -> None:
-    """Called each time the user opens a different module."""
-    # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
-    if self._parameterNode:
-      self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
-      self._parameterNodeGuiTag = None
-
-  def onSceneStartClose(self, caller, event) -> None:
-    """Called just before the scene is closed."""
-    # Parameter node will be reset, do not use it anymore
-    self.setParameterNode(None)
-
-  def onSceneEndClose(self, caller, event) -> None:
-    """Called just after the scene is closed."""
-    # If this module is shown while the scene is closed then recreate a new parameter node immediately
-    if self.parent.isEntered:
-      self.initializeParameterNode()
-
-  def initializeParameterNode(self) -> None:
-    """Ensure parameter node exists and observed."""
-    # Parameter node stores all user choices in parameter values, node selections, etc.
-    # so that when the scene is saved and reloaded, these settings are restored.
-
-    self.setParameterNode(self.logic.getParameterNode())
-
-  def setParameterNode(self, inputParameterNode: Optional[CreateFantomModuleParameterNode]) -> None:
-    """
-    Set and observe parameter node.
-    Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
-    """
-
-    if self._parameterNode:
-      self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
-    self._parameterNode = inputParameterNode
-    if self._parameterNode:
-      # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
-      # ui element that needs connection.
-      self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
 
   def onTrimesterOrHeadChange(self) -> None:
     # if trimester changes, this can change the height range
@@ -342,6 +264,103 @@ class CreateFantomModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
   def segementationComboIndexChange(self, index) -> None:
     text = self.ui.segmentationListComboBox.itemText(index)
     self.setSelectedSegmentationsFromPreset(text)
+
+
+class CreateFantomModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent=None) -> None:
+    """Called when the user opens the module the first time and the widget is initialized."""
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    VTKObservationMixin.__init__(self)  # needed for parameter node observation
+    self.logic = None
+    self._parameterNode = None
+    self._parameterNodeGuiTag = None
+
+  def setup(self) -> None:
+    """Called when the user opens the module the first time and the widget is initialized."""
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Load widget from .ui file (created by Qt Designer).
+    # Additional widgets can be instantiated manually and added to self.layout.
+    uiWidget = slicer.util.loadUI(self.resourcePath("UI/CreateFantomModule.ui"))
+    self.layout.addWidget(uiWidget)
+    self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+    # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+    # "setMRMLScene(vtkMRMLScene*)" slot.
+    uiWidget.setMRMLScene(slicer.mrmlScene)
+
+    # Create logic class. Logic implements all computations that should be possible to run
+    # in batch mode, without a graphical user interface.
+    self.logic = CreateFantomModuleLogic()
+
+    # Make sure parameter node is initialized (needed for module reload)
+    self.initializeParameterNode()
+
+    # Create tab widgets, inject into tabs
+    self.generateFantomTabUI = GenerateFantomTabWidget(self.logic, self.resourcePath)
+    # self.blendSegmentationsTabUI = GenerateFantomTabWidget(self.logic, self.resourcePath)
+    # self.segmentToFantomTabUI = GenerateFantomTabWidget(self.logic, self.resourcePath)
+    self.ui.generateFantomTab.layout().addWidget(self.generateFantomTabUI.widget)
+    # self.ui.blendSegmentationsTab.layout().addWidget(self.blendSegmentationsTabUI.widget)
+    # self.ui.segmentToFantomTab.layout().addWidges(self.segmentToFantomTabUI.widget)
+
+    # These connections ensure that we update parameter node when scene is closed
+    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+    self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+  def cleanup(self) -> None:
+    """Called when the application closes and the module widget is destroyed."""
+    self.removeObservers()
+
+  def enter(self) -> None:
+    """Called each time the user opens this module."""
+    # Make sure parameter node exists and observed
+    self.initializeParameterNode()
+
+  def exit(self) -> None:
+    """Called each time the user opens a different module."""
+    # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
+    if self._parameterNode:
+      self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+      self._parameterNodeGuiTag = None
+
+  def onSceneStartClose(self, caller, event) -> None:
+    """Called just before the scene is closed."""
+    # Parameter node will be reset, do not use it anymore
+    self.setParameterNode(None)
+
+  def onSceneEndClose(self, caller, event) -> None:
+    """Called just after the scene is closed."""
+    # If this module is shown while the scene is closed then recreate a new parameter node immediately
+    if self.parent.isEntered:
+      self.initializeParameterNode()
+
+  def initializeParameterNode(self) -> None:
+    """Ensure parameter node exists and observed."""
+    # Parameter node stores all user choices in parameter values, node selections, etc.
+    # so that when the scene is saved and reloaded, these settings are restored.
+
+    self.setParameterNode(self.logic.getParameterNode())
+
+  def setParameterNode(self, inputParameterNode: Optional[CreateFantomModuleParameterNode]) -> None:
+    """
+    Set and observe parameter node.
+    Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+    """
+
+    if self._parameterNode:
+      self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+    self._parameterNode = inputParameterNode
+    if self._parameterNode:
+      # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
+      # ui element that needs connection.
+      self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
+
 
 class CreateFantomModuleLogic(ScriptedLoadableModuleLogic):
   """This class should implement all the actual
@@ -785,3 +804,4 @@ class CreateFantomModuleLogic(ScriptedLoadableModuleLogic):
     slicer.mrmlScene.RemoveNode(cliNode)
 
     return
+
